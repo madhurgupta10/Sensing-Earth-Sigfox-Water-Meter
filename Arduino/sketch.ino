39f93b90a8f187035ad05f0125d4400709eac80e
Madhur Gupta <madhurgupta10@gmail.com>
2018-07-31 23:40:41 +0530

    Updated Arduino Code

--- Arduino/sketch.ino
+++ Arduino/sketch.ino
@@ -1,11 +1,3 @@
-/*
- # This code is adapted from Sample Code Provided at
- https://www.dfrobot.com/wiki/index.php/PH_meter(SKU:_SEN0161)
- # Editor : YouYou
- # Ver    : 1.0
- # Product: analog pH meter
- # SKU    : SEN0161
-*/
 #define SensorPin A0 // pH meter Analog output to Arduino Analog Input 0
 #define Offset 0.00  // deviation compensate
 #define samplingInterval 20
@@ -16,26 +8,35 @@ int pHArrayIndex = 0;
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.println("pH meter experiment!"); // Test the serial monitor
 }
 
 void loop(void) {
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
   static float pHValue, voltage;
+  int sensorValue;
+  float turbidity_voltage;
+
   if (millis() - samplingTime > samplingInterval) {
     pHArray[pHArrayIndex++] = analogRead(SensorPin);
-    if (pHArrayIndex == ArrayLenth)
+    if (pHArrayIndex == ArrayLenth) {
       pHArrayIndex = 0;
+    }
     voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
     pHValue = 3.5 * voltage + Offset;
     samplingTime = millis();
   }
   if (millis() - printTime > printInterval) { // Every 800 milliseconds, print a numerical
-    Serial.print("pH ");
+    Serial.print("{");
+    Serial.print("pH: ");
     Serial.print(pHValue, 2);
-    Serial.print("\n");
+    Serial.print(",");
     printTime = millis();
+    sensorValue = analogRead(A1); // read the input on analog pin 1:
+    turbidity_voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+    Serial.print("Turbidity_Voltage: ");
+    Serial.print(turbidity_voltage); // print out the value you read:
+    Serial.print("}\n");
   }
 }
 
